9b8753dc3bc761c87aeb9ffe71a31a1e61a4fa45
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-17 19:35:50 -0600

    Add purpose audience and goal.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,32 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-		<title>Purpose, Audience, Goal</title>
+		<title>PWP Milestone 1</title>
 	</head>
 	<body>
+		<h1>Purpose, Audience, and Goal</h1>
+		<h4>Purpose:</h4>
+		<p>
+			The purpose of my PWP is to create a personal website that highlights my skills in web development to help me find gainful employment in the Albuquerque area.
+		</p>
+		<h4>Audience:</h4>
+		<p>
+			This site is targeted towards hiring professionals looking for web developers in the Albuqureque area. The target audience includes hiring managers, recruiters, and peers in the tech industry.
+		</p>
+		<h4>Goal:</h4>
+		<p>
+			The main goal of my site is to initiate contact with users of my site regarding opportunities for employment. My site will house a portfolio of my professional web development work that will help users become acquainted with my skillset.
+		</p>
+		<h1>Persona</h1>
+			<p><strong>Name: </strong>Millicent Powers</p>
+			<p><strong>Age: </strong>41</p>
+			<p><strong>Gender: </strong>Female</p>
+			<p><strong>Profession: </strong>Managing Director of Urban Web Development’s Albuquerque office, which is looking to hire a full time web developer. Urban Web Development opened its branch office in downtown Albuquerque 5 years ago and business has outgrown their current staffing levels with clients ranging from local businesses to large international web publications.</p>
+			<p><strong>Technology: </strong>Millicent is platform agnostic. She uses Mac, Linux, and Android user. Uses a MacBook Pro with 2 27” LG 5k displays while in the office, a Dell XPS laptop running Ubuntu 18.04 on a 3840 x 2160 display that is extended to a 34” 4k display in her home office, a 10.5” iPad Pro for browsing the web around the house, and her phone is a Pixel 2 XL with an unlimited LTE data connection.<p/>
+			<p><strong>Attitudes and Behaviors: </strong> </p>
+
+
+
 
 	</body>
 </html>
