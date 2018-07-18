4d442f0ce153f94e8a3dc0779d4a294497dd6d52
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-17 19:44:44 -0600

    Add persona. Add PWP Milestone 1 as H1 tag.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,29 +5,20 @@
 		<title>PWP Milestone 1</title>
 	</head>
 	<body>
-		<h1>Purpose, Audience, and Goal</h1>
-		<h4>Purpose:</h4>
-		<p>
-			The purpose of my PWP is to create a personal website that highlights my skills in web development to help me find gainful employment in the Albuquerque area.
-		</p>
-		<h4>Audience:</h4>
-		<p>
-			This site is targeted towards hiring professionals looking for web developers in the Albuqureque area. The target audience includes hiring managers, recruiters, and peers in the tech industry.
-		</p>
-		<h4>Goal:</h4>
-		<p>
-			The main goal of my site is to initiate contact with users of my site regarding opportunities for employment. My site will house a portfolio of my professional web development work that will help users become acquainted with my skillset.
-		</p>
+		<h1>PWP Milestone 1</h1>
+		<h2>Purpose, Audience, and Goal</h2>
+			<p><strong>Purpose: </strong>The purpose of my PWP is to create a personal website that highlights my skills in web development to help me find gainful employment in the Albuquerque area.</p>
+			<p><strong>Audience: </strong>This site is targeted towards hiring professionals looking for web developers in the Albuqureque area. The target audience includes hiring managers, recruiters, and peers in the tech industry.</p>
+			<p><strong>Goal: </strong>The main goal of my site is to initiate contact with users of my site regarding opportunities for employment. My site will house a portfolio of my professional web development work that will help users become acquainted with my skillset.</p>
 		<h1>Persona</h1>
 			<p><strong>Name: </strong>Millicent Powers</p>
 			<p><strong>Age: </strong>41</p>
 			<p><strong>Gender: </strong>Female</p>
 			<p><strong>Profession: </strong>Managing Director of Urban Web Development’s Albuquerque office, which is looking to hire a full time web developer. Urban Web Development opened its branch office in downtown Albuquerque 5 years ago and business has outgrown their current staffing levels with clients ranging from local businesses to large international web publications.</p>
-			<p><strong>Technology: </strong>Millicent is platform agnostic. She uses Mac, Linux, and Android user. Uses a MacBook Pro with 2 27” LG 5k displays while in the office, a Dell XPS laptop running Ubuntu 18.04 on a 3840 x 2160 display that is extended to a 34” 4k display in her home office, a 10.5” iPad Pro for browsing the web around the house, and her phone is a Pixel 2 XL with an unlimited LTE data connection.<p/>
-			<p><strong>Attitudes and Behaviors: </strong> </p>
-
-
-
-
+			<p><strong>Technology: </strong>Millicent is platform agnostic. She uses Mac, Linux, and Android user. Uses a MacBook Pro with 2 27” LG 5k displays while in the office, a Dell XPS laptop running Ubuntu 18.04 on a 3840 x 2160 display that is extended to a 34” 4k display in her home office, a 10.5” iPad Pro for browsing the web around the house, and her phone is a Pixel 2 XL with an unlimited LTE data connection.</p>
+			<p><strong>Attitudes and Behaviors: Millicent, is very busy with a growing Albuquerque office and a family at home. She does not have much time to spend looking at portfolio sites before having her secretary set up interviews, therefore portfolio sites need to be professional, informative, and attention grabbing.</strong> </p>
+			<p><strong>Frustrations and Needs: </strong>Millicent needs to hire someone soon with a proven skill set that is willing to take an entry level web developer job to help take on the increasing workload. She needs someone that is a good cultural fit for the office and can work with the existing development team and hit the ground running and take on their share of web development projects.</p>
+			<p><strong>Goals: </strong>Millicent is looking to find potential candidates for new web development positions that she has created due to an increasing number of projects and clients. Because she is still focused on managing her current staff, and helping attract future work, she can spend less than 4 minutes looking at someone’s personal site and linked to determine if they should be invited for an interview.</p>
+			<p><strong>User Story: </strong>“As a user, I am looking to use someone’s personal website to help determine whether they are a candidate for an entry level web development position.”</p>
 	</body>
 </html>
