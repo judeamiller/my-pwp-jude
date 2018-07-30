a1cdd34973f304c807380f4dcbbd8bd2ee485ce9
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-30 13:54:04 -0600

    fill in portfolio, contact, and footer sections of content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,9 +9,9 @@
 		<h2>Content Strategy</h2>
 		<p><strong>Header: </strong>My header will clearly state my name and my role as a web developer. The left side of my header will include page navigation links. The nav links will be collapsed into a "hamburger menu" for mobile, but  will be expanded for larger screens.</p>
 		<p><strong>About Me: </strong>This section will describe my skillset, work history, personality. I want to use a personable/casual tone for this section.</p>
-		<p><strong>Portfolio:</strong></p>
-		<p><strong>Contact: </strong></p>
-		<p><strong>Footer:</strong></p>
+		<p><strong>Portfolio:</strong> this section will be an image gallery. The user can tap on one of the 3-6 categories represented by the circular photos to open up an image gallery where the user can then scan through a slideshow that has captions thtat describe the project.</p>
+		<p><strong>Contact: </strong>This section contains a simple contact form where users can enter their name, email address and a message. The user will use the reCAPTCHA form before submitting their message.</p>
+		<p><strong>Footer:</strong> Will contain links to my Resumé, and LinkedIn and GitHub accounts.</p>
 		<h2>Wireframes</h2>
 
 	</body>
