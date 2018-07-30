98aaefafd9a1ad0c9226f58618438ce3f1dbca08
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-30 13:51:18 -0600

    add content strategy framework

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,5 +6,13 @@
 	</head>
 	<body>
 		<h1>PWP Milestone 2a</h1>
+		<h2>Content Strategy</h2>
+		<p><strong>Header: </strong>My header will clearly state my name and my role as a web developer. The left side of my header will include page navigation links. The nav links will be collapsed into a "hamburger menu" for mobile, but  will be expanded for larger screens.</p>
+		<p><strong>About Me: </strong>This section will describe my skillset, work history, personality. I want to use a personable/casual tone for this section.</p>
+		<p><strong>Portfolio:</strong></p>
+		<p><strong>Contact: </strong></p>
+		<p><strong>Footer:</strong></p>
+		<h2>Wireframes</h2>
+
 	</body>
 </html>
