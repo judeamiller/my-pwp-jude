44e4266d46d9090e57f6b6346d7836db4118ef1b
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-30 21:53:43 -0600

    add wireframe images

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,8 +11,20 @@
 		<p><strong>About Me: </strong>This section will describe my skillset, work history, personality. I want to use a personable/casual tone for this section.</p>
 		<p><strong>Portfolio:</strong> this section will be an image gallery. The user can tap on one of the 3-6 categories represented by the circular photos to open up an image gallery where the user can then scan through a slideshow that has captions thtat describe the project.</p>
 		<p><strong>Contact: </strong>This section contains a simple contact form where users can enter their name, email address and a message. The user will use the reCAPTCHA form before submitting their message.</p>
-		<p><strong>Footer:</strong> Will contain links to my Resumé, and LinkedIn and GitHub accounts.</p>
+		<p><strong>Footer:</strong> Will contain links to my resumé, and LinkedIn and GitHub accounts.</p>
 		<h2>Wireframes</h2>
+		<h3>Mobile First</h3>
+		<img src="./images/mobilesite.png" alt="mobile site wireframes"/>
+		<p>Above are my mobile site wireframes. The left image shows what the site will look like with when the page loads.
+		The second image is of the expanded "hamburger" menu. The third image shows the page scrolled down to the portfolio section.
+		The fourth image shows the gallery view once one of the images has been tapped. The fifth image shows the page scrolled down to the contact section.</p>
+		<h3>Desktop Browser View</h3>
+		<img src="./images/desktoptoppage.png" alt="wireframe of pwp site loaded"/>
+		<p>The wireframe sketch above shows what the site will look like loaded in a desktop browser. Here you can see my header, about me section, and portfolio section.</p>
+		<img src="./images/desktopgallery.png" alt="desktop portfolio gallery wireframe view"/>
+		<p>This wireframe shows the gallery view after one of the the portfolio pictures has been clicked.</p>
+		<img src="./images/desktopscrolldown.png" alt="wireframe view of the desktop version of the contact section"/>
+		<p>This image shows  the page scrolled down to the bottom. Here you can see the full "contact" section as well as the footer with clickable links to download my resume, or navigate to my LinkedIn or GitHub accounts.</p>
 
 	</body>
 </html>
