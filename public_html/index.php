936744c2d9591a94439b33d2bc5c17cdc2946517
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-06 21:46:13 -0600

    add bootstrap and local css to head tag. Begin building bootstrap header.

---
+++ public_html/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+		<!--bootstrap-->
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+
+		<!--local stylesheet-->
+		<link rel="stylesheet" href="./css/style.css"/>
+
+		<title>Jude's Personal Website</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+		<a class="navbar-brand" href="#">Jude Baca-Miller <br>Web Developer</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#homework">About Me<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#reflections">Portfolio</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="https://www.linkedin.com/in/judeamiller/" target="_blank">Contact Me</a>
+				</li>
+								<li class="nav-item">
+					<a class="nav-link" href="Baca-Miller-Resume.pdf" target="_blank">Resume</a>
+				</li>
+			</ul>
+		</div>
+		</nav>
+		<main>
+
+		</main>
+	</body>
+</html>
