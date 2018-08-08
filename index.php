99df134c30176a231f3f99701783cb6fced4be64
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-07 18:41:35 -0600

    change linked font to open sans

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
 
 		<!--	fonts	-->
-		<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 		<!--local stylesheet-->
 		<link rel="stylesheet" href="public_html/css/style.css"/>
